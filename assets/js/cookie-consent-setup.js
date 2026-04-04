aca33f43d32c8bacc98fe1e0bdee30757965460d
alexchen4ai <52059011+alexchen4ai@users.noreply.github.com>
2026-04-04 17:34:18 +0000

    Deploying to gh-pages from @ alexchen4ai/blog@383b91017a1a6a4e52c2f9f0aac2381295d535d8 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://alshedivat.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://alexchen.ai/#contact">contact us</a>.'
               }
             ]
           }
